3c5923fee40541685e7d8d03f681884643da8a3f
Sean B <strawhorse@gmail.com>
2023-12-29 14:52:08 +0000

    Created labels

--- src/myrestaurant/MyRestaurant.java
+++ src/myrestaurant/MyRestaurant.java
@@ -1,9 +1,11 @@
 package myrestaurant;
 
+import java.awt.Dimension;
 import java.awt.HeadlessException;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JTextField;
+import javax.swing.WindowConstants;
 
 /**
  *
@@ -19,13 +21,27 @@ public class MyRestaurant extends JFrame{
     public MyRestaurant() throws HeadlessException {
     
         //        Create inital frame for app
-        setTitle("My Restaurant");
-        setSize(600,500);
-        
+       setTitle("My Restaurant");
+//        setMinimumSize(minimumSize);
+       setSize(600,500);
+       setLayout(null);
+                
+//       set initial labels
        Customer = new JLabel("Customer No: ");
        Customer.setBounds(20, 20, 120, 30);
        
+       Name = new JLabel("Customer Name: ");
+       Name.setBounds(20, 50, 120, 30);
+       
+       Contact = new JLabel("Customer Contact: ");
+       Contact.setBounds(20, 80, 120, 30);
+       
+       getContentPane().add(Customer);
+       getContentPane().add(Name);
+       getContentPane().add(Contact);
 
+       setVisible(true);
+       setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
         
     }
     
@@ -34,8 +50,7 @@ public class MyRestaurant extends JFrame{
 
     public static void main(String[] args) {
         
-
-        
+        new MyRestaurant();
         
         
     }
