de254d48de2fbc4b9b282f188b7a76f0ec8f406b
Sean B <strawhorse@gmail.com>
2023-12-29 18:39:56 +0000

    Finish off initial buttons

--- src/myrestaurant/MyRestaurant.java
+++ src/myrestaurant/MyRestaurant.java
@@ -1,6 +1,5 @@
 package myrestaurant;
 
-import java.awt.Dimension;
 import java.awt.HeadlessException;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
@@ -29,16 +28,26 @@ public class MyRestaurant extends JFrame{
 //       set initial labels
        Customer = new JLabel("Customer No: ");
        Customer.setBounds(20, 20, 120, 30);
+       tfNumber = new JTextField();
+       tfNumber.setBounds(150, 20, 300, 30);
        
        Name = new JLabel("Customer Name: ");
-       Name.setBounds(20, 50, 120, 30);
+       Name.setBounds(20, 80, 120, 30);
+       tfName = new JTextField();
+       tfName.setBounds(150, 80, 300, 30);
        
        Contact = new JLabel("Customer Contact: ");
-       Contact.setBounds(20, 80, 120, 30);
+       Contact.setBounds(20, 140, 120, 30);
+       tfContact = new JTextField();
+       tfContact.setBounds(150, 140, 300, 30);
        
+//       add all the content panes; can be done quicker by adding them all to a panel and then adding the panel to the frame
        getContentPane().add(Customer);
        getContentPane().add(Name);
        getContentPane().add(Contact);
+       getContentPane().add(tfNumber);
+       getContentPane().add(tfContact);
+       getContentPane().add(tfName);
 
        setVisible(true);
        setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
