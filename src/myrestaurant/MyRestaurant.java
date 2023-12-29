b8c366421001c4c8e7499cd0525841b32263ff76
Sean B <strawhorse@gmail.com>
2023-12-29 13:43:17 +0000

    Adding panel

--- src/myrestaurant/MyRestaurant.java
+++ src/myrestaurant/MyRestaurant.java
@@ -1,6 +1,9 @@
 package myrestaurant;
 
+import java.awt.FlowLayout;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
 
 /**
  *
@@ -13,8 +16,15 @@ public class MyRestaurant {
         
 //        Create inital frame for app
         JFrame frame = new JFrame("My Restaurant");
+        JLabel label = new JLabel("My restaurant");
+        JPanel panel = new JPanel(new FlowLayout(FlowLayout.LEFT));
+        panel.add(label);
+        frame.add(panel);
+        
+
         frame.setSize(300,200);
         frame.setVisible(true);
+        
         
         
         
